9370c3c1acd0ca1d05c10d46e092215e5aa57250
Oli <N1185123@my.ntu.ac.uk>
2026-08-21 16:53:21 +0100

    Defined Allele Types

--- Evolution/Allele.cs
+++ Evolution/Allele.cs
@@ -1,21 +1,61 @@
-class Allele
+enum BodyShapes
 {
-  
-
-  
+  TwoLegs,
+  FourLegs
 }
+
+enum RGBColours
+{
+  Red,
+  Green,
+  Blue
+}
+
+enum EyeShape
+{
+  OneEye,
+  TwoEyes,
+  ThreeEyes
+}
+
+enum SizeValues
+{
+  Small,
+  Medium,
+  Large
+}
+
+class Allele{}
 
 class ColourAllele : Allele
 {
-  
+  ColourAllele(RGBColours _colour, Byte _colourPair)
+  {
+    colour = _colour;
+    colourPair = _colourPair;
+  }
+
+  public RGBColours colour
+  {get;}
+  private Byte colourPair
+  {get;}
+
 }
 
-class EyeAllele : Allele
+class BodyPartAllele : Allele
 {
-  
-}
+  BodyPartAllele(System.Enum _bodyPartType, SizeValues _bodyPartSize)
+  {
+    bodyPartType = _bodyPartType;
+    bodyPartSize = _bodyPartSize;
+  }
 
-class BodyAllele : Allele
-{
-  
+  public Type AlleleType()
+  {
+    return bodyPartType.GetType();
+  }
+  public System.Enum bodyPartType
+  {get;}
+  public SizeValues bodyPartSize
+  {get;}
 }
