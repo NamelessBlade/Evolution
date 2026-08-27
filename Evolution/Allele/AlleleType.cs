7f8dc0bfd37a21b29fbefaa8dc7bfd67689c1561
Oli <N1185123@my.ntu.ac.uk>
2026-08-27 13:58:58 +0100

    Reapply "Updated file structure to split Allele classes across seperate files"
    This reverts commit 2ece755d8287e73e1485dd34aab993ccf7707dc3.

---
+++ Evolution/Allele/AlleleType.cs
@@ -0,0 +1,10 @@
+namespace Evolution.Allele;
+
+enum AlleleType
+{
+    Body,
+    Eye,
+    Red,
+    Blue,
+    Green
+}
---
+++ Evolution/Allele/BodyShapes.cs
@@ -0,0 +1,7 @@
+namespace Evolution.Allele;
+
+enum BodyShapes : int
+{
+    TwoLegs = 0,
+    FourLegs = 1
+}
